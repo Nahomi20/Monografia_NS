3098fe16e282a107d9ecdc1ab02374b9ad84a95c
Nahomi Salazar <Nahomi Salazar@DESKTOP-07FGBQ6>
2026-01-31 13:38:31 -0600

    Initial Commit

---
+++ GestionAcademica/Controllers/Dashboard/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestionAcademica.Controllers.Dashboard
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
